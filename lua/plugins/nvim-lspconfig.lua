94c0d074a1a108c6c5290d0ed755b385fb42481d
wijipermana <wiji.permana@gmail.com>
2021-11-06 17:28:59 +0700

    getting started with lsp

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,13 +1,5 @@
 local nvim_lsp = require('lspconfig')
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-require'lspconfig'.pyright.setup{}
-require'lspconfig'.html.setup {
-  capabilities = capabilities,
-}
-
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
@@ -52,3 +44,12 @@ for _, lsp in ipairs(servers) do
     }
   }
 end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require'lspconfig'.html.setup {
+  capabilities = capabilities,
+}
+
+require'lspconfig'.tsserver.setup{}
