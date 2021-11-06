77bf0ff013a80cae44599de1cc89191ba5d75762
wijipermana <wiji.permana@gmail.com>
2021-11-06 20:16:37 +0700

    edit plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,5 @@
 require('plugins.packer')
 require('plugins.emmet')
 require('plugins.indent-blankline')
-require('plugins.delimitMate')
+require('plugins.nvim-autopairs')
 require('plugins.nvim-lspconfig')
