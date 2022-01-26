a1cd6b55e3e18cb114afbcdf6f07b18c9a1b8549
wiji <wiji.permana@gmail.com>
2022-01-26 13:29:17 +0700

    update config

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -72,8 +72,8 @@ cmp.setup.cmdline(':', {
   })
 
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+-- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-require('lspconfig').tsserver.setup {
-  capabilities = capabilities
-}
+-- require('lspconfig').tsserver.setup {
+--   capabilities = capabilities
+-- }
